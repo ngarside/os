0e34607f657f5ab0d3df4ec85d5bfc54adcd57bb
Nathan Garside <git@nathangarside.com>
2024-07-05 08:10:15 +0100

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -8,6 +8,12 @@ rpm-ostree override remove yelp yelp-xsl yelp-libs
 
 rpm-ostree override remove gnome-tour
 
+rpm-ostree override remove gnome-terminal gnome-terminal-nautilus
+
+rpm-ostree override remove firefox firefox-langpacks
+
+rpm-ostree install gnome-console
+
 # flatpak remove --all --delete-data --noninteractive
 
 rpm-ostree cleanup --repomd
