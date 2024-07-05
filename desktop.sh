839ad103053238204130fcc3c3393a2f90b104c1
Nathan Garside <git@nathangarside.com>
2024-07-05 07:44:11 +0100

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,6 +1,8 @@
 # This is free and unencumbered software released into the public domain.
 
-rpm-ostree override remove gnome-shell-extension-common
+rpm-ostree override remove gnome-classic-session gnome-classic-session-xsession
+
+# rpm-ostree override remove gnome-shell-extension-common
 
 rpm-ostree override remove yelp yelp-xsl yelp-libs
 
