337871409c9b0e6e3d0ac3cded4b29622572ea69
Nathan Garside <git@nathangarside.com>
2024-07-05 07:35:51 +0100

    a

--- desktop.sh
+++ desktop.sh
@@ -1,6 +1,8 @@
 # This is free and unencumbered software released into the public domain.
 
-rpm-ostree override remove yelp
+rpm-ostree override remove gnome-shell-extension-common
+
+rpm-ostree override remove yelp yelp-xsl yelp-libs
 
 rpm-ostree cleanup --repomd
 
