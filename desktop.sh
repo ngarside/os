e4a4dbfd85c45a49785a92913fa9c9db98381b24
Nathan Garside <git@nathangarside.com>
2024-07-09 07:28:38 +0100

    a

--- desktop.sh
+++ desktop.sh
@@ -2,8 +2,6 @@
 
 rpm-ostree override remove gnome-classic-session gnome-classic-session-xsession
 
-# rpm-ostree override remove gnome-shell-extension-common
-
 rpm-ostree override remove yelp yelp-xsl yelp-libs
 
 rpm-ostree override remove gnome-tour
@@ -25,12 +23,6 @@ rpm-ostree override remove fedora-bookmarks fedora-workstation-backgrounds
 rpm-ostree override remove gnome-shell-extension-common gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo
 
 rpm-ostree install gnome-console
-
-# flatpak remove --all --delete-data --noninteractive
-
-# gsettings set org.gnome.shell app-picker-layout "[]"
-
-# flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 
 rpm-ostree cleanup --repomd
 
