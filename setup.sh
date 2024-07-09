5b7773bf2ed9ce3c11ba3bcd9c0dea0f3caa9cc0
Nathan Garside <git@nathangarside.com>
2024-07-09 07:38:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 # This is free and unencumbered software released into the public domain.
 
 rpm-ostree override remove \
-	gnome-classic-session \
-	gnome-classic-session-xsession \
-	yelp \
-	yelp-xsl \
-	yelp-libs \
-	gnome-tour \
-	gnome-terminal gnome-terminal-nautilus \
-	firefox firefox-langpacks \
-	gnome-system-monitor \
-	gnome-disk-utility \
-	git-core git-core-doc \
-	virtualbox-guest-additions \
 	fedora-bookmarks \
 	fedora-workstation-backgrounds \
-	gnome-shell-extension-common \
+	firefox firefox-langpacks \
+	git-core git-core-doc \
+	gnome-classic-session \
+	gnome-classic-session-xsession \
+	gnome-disk-utility \
 	gnome-shell-extension-apps-menu \
+	gnome-shell-extension-background-logo
+	gnome-shell-extension-common \
 	gnome-shell-extension-launch-new-instance \
 	gnome-shell-extension-places-menu \
 	gnome-shell-extension-window-list \
-	gnome-shell-extension-background-logo
+	gnome-system-monitor \
+	gnome-terminal gnome-terminal-nautilus \
+	gnome-tour \
+	virtualbox-guest-additions \
+	yelp \
+	yelp-libs \
+	yelp-xsl \
 
 rpm-ostree install gnome-console
 
