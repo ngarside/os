0388918e543074d854940d79b060ab2074720869
Nathan Garside <git@nathangarside.com>
2024-07-09 07:38:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 # This is free and unencumbered software released into the public domain.
 
-rpm-ostree override remove gnome-classic-session gnome-classic-session-xsession
-
-rpm-ostree override remove yelp yelp-xsl yelp-libs
-
-rpm-ostree override remove gnome-tour
-
-rpm-ostree override remove gnome-terminal gnome-terminal-nautilus
-
-rpm-ostree override remove firefox firefox-langpacks
-
-rpm-ostree override remove gnome-system-monitor
-
-rpm-ostree override remove gnome-disk-utility
-
-rpm-ostree override remove git-core git-core-doc
-
-rpm-ostree override remove virtualbox-guest-additions
-
-rpm-ostree override remove fedora-bookmarks fedora-workstation-backgrounds
-
-rpm-ostree override remove gnome-shell-extension-common gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo
+rpm-ostree override remove \
+	gnome-classic-session \
+	gnome-classic-session-xsession \
+	yelp \
+	yelp-xsl \
+	yelp-libs \
+	gnome-tour \
+	gnome-terminal gnome-terminal-nautilus \
+	firefox firefox-langpacks \
+	gnome-system-monitor \
+	gnome-disk-utility \
+	git-core git-core-doc \
+	virtualbox-guest-additions \
+	fedora-bookmarks \
+	fedora-workstation-backgrounds \
+	gnome-shell-extension-common \
+	gnome-shell-extension-apps-menu \
+	gnome-shell-extension-launch-new-instance \
+	gnome-shell-extension-places-menu \
+	gnome-shell-extension-window-list \
+	gnome-shell-extension-background-logo
 
 rpm-ostree install gnome-console
 
